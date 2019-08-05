9ed33469f4b69ca530049424374642b14c5b4a02
Hongli (Bob) Zhao <44326880+honglizhaobob@users.noreply.github.com>
2019-08-05 10:46:50 +0800

    Delete Q12_code.m

--- Numerical Solutions to ODEs/HW02/Q12_code.m
+++
@@ -1,8 +0,0 @@
-% call bisection with arguments
-% we test t = 10^-10, bisecting the interval [-1,2]
-[approx_root, history] = bisection(-1,2,@(x,p)(atan(x-0.111) ),10,10^-10);
-
-final_r = approx_root;
-exact = 0.111;
-abs_e = abs(final_r - exact);
-rel_e = abs_e / exact;
